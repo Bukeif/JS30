f9037db193534a7da7a202e665c541ec48ebde80
Bukeif <az4659811@gmail.com>
2024-02-28 23:29:28 +0800

    add 06 Type Ahead and edit 03 05 html

--- js/nav.js
+++ js/nav.js
@@ -28,10 +28,10 @@ let nav = `
                             <a class="item__link" href="../05-Flex Panels Gallery/index.html">Flex Panels Gallery</a>
                         </li>
                         <li class="menu__item dropdown__subItem">
-                            <a class="item__link">3~10</a>
+                            <a class="item__link">06~10</a>
                             <ul class="dropdown__subMenu">
                                 <li class="menu__item">
-                                    <a class="item__link" href="../02-JSDrumkit/index.html">3</a>
+                                    <a class="item__link" href="../06-Type Ahead/index.html">Type Ahead</a>
                                 </li>
                                 <li class="menu__item">
                                     <a class="item__link">4</a>
